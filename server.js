6228416547ab38cb1b54e321ad1690c9495c8554
danila <danila.kononov@netcracker.com>
2022-05-02 16:49:37 +0400

    js lesson 7

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.use(express.json()) // for parsing application/json
+const products = [
+    {id_product: 123, product_name: "Ноутбук", price: 45600},
+    {id_product: 456, product_name: "Мышка", price: 1000}
+    ]
+
+let cart = []
+
+app.get('/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/cart', (req, res) => {
+    res.json(cart)
+})
+
+app.post('/addProduct', (req, res) => {
+    const pr = req.body
+    cart.push(pr)
+    res.json(pr)
+})
+
+app.delete('/deleteProduct', (req, res) => {
+    cart = cart.filter(x => x.id_product !== req.body.id_product)
+    res.end();
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
